266e1a00f20ae1c004b4ccb793db3299b2abe0d9
Elvin <qarayevelvin109@gmail.com>
2022-10-11 10:39:56 +0400

    ikinci taskimi bitirdim.

--- src/Gun1/Merhaba.java
+++ src/Gun1/Merhaba.java
@@ -2,12 +2,16 @@ package Gun1;
 
 public class Merhaba {
     public static void main(String[] args) {
-        System.out.println("Merhaba");
+
 
         for (int i = 0; i <5 ; i++) {
             System.out.println("Merhaba"+i);
 
         }
-
+        int sayac=0;
+        do{
+            System.out.println("Yeni merhaba");
+            sayac++;
+        }while (sayac<5);
     }
 }
